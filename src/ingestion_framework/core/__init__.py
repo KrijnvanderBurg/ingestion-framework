7ccb662ca23b43d0fd129884bfdd09fccf972c89
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-06-05 20:46:23 +0000

    wip

--- src/ingestion_framework/core/__init__.py
+++ src/ingestion_framework/core/__init__.py
@@ -2,12 +2,13 @@
 Core functionality for the ingestion framework.
 """
 
-import ingestion_framework.core.transforms
 from ingestion_framework.core.transform import Function, Transform, TransformFunctionRegistry, TransformRegistry
+from ingestion_framework.core.transforms import SelectFunction
 
 __all__ = [
     "Function",
     "Transform",
     "TransformFunctionRegistry",
     "TransformRegistry",
+    "SelectFunction",
 ]
