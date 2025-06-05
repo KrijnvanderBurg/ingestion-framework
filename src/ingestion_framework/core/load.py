be9b41697acfaf666ac031b160395cf18adbb8dd
KrijnvanderBurg <32356726+KrijnvanderBurg@users.noreply.github.com>
2025-06-05 17:25:12 +0000

    wip

--- src/ingestion_framework/core/load.py
+++ src/ingestion_framework/core/load.py
@@ -7,12 +7,11 @@ to various destinations and formats.
 
 import json
 from abc import ABC, abstractmethod
-from enum import Enum
 from typing import Any, Final, Generic, Self, TypeVar
 
 from pyspark.sql.streaming.query import StreamingQuery
 
-from ingestion_framework.exceptions import DictKeyError
+from ingestion_framework.models.load import LoadFormat, LoadMethod, LoadModel, LoadModelFile
 from ingestion_framework.types import DataFrameRegistry, RegistryDecorator, Singleton, StreamingQueryRegistry
 from ingestion_framework.utils.spark import SparkHandler
 
@@ -25,7 +24,7 @@ LOCATION: Final[str] = "location"
 SCHEMA_LOCATION: Final[str] = "schema_location"
 OPTIONS: Final[str] = "options"
 
-LoadModelT = TypeVar("LoadModelT", bound="LoadModel")
+LoadModelT = TypeVar("LoadModelT", bound=LoadModel)
 
 
 class LoadRegistry(RegistryDecorator, metaclass=Singleton):
@@ -34,145 +33,6 @@ class LoadRegistry(RegistryDecorator, metaclass=Singleton):
 
     Maps ExtractFormat enum values to concrete ExtractAbstract implementations.
     """
-
-
-class LoadMethod(Enum):
-    """Enumeration for methods of load modes."""
-
-    BATCH = "batch"
-    STREAMING = "streaming"
-
-
-class LoadMode(Enum):
-    """Enumeration for methods of load modes."""
-
-    # pyspark batch
-    APPEND = "append"
-    OVERWRITE = "overwrite"
-    ERROR = "error"
-    ERROR_IF_EXISTS = "errorifexists"
-    IGNORE = "ignore"
-
-    # pyspark streaming
-    # APPEND = "append"  # already added above for pyspark batch
-    COMPLETE = "complete"
-    UPDATE = "update"
-
-
-class LoadFormat(Enum):
-    """Enumeration for methods of input and structures for the load."""
-
-    PARQUET = "parquet"
-    JSON = "json"
-    CSV = "csv"
-
-
-class LoadModel(ABC):
-    """
-    Abstract base class for load operation models.
-
-    This class defines the configuration model for data loading operations,
-    specifying the name, upstream source, method, and destination for the load.
-    """
-
-    def __init__(
-        self,
-        name: str,
-        upstream_name: str,
-        method: LoadMethod,
-        location: str,
-        schema_location: str | None,
-        options: dict[str, str],
-    ) -> None:
-        """
-        Initialize LoadModelAbstract with the modelified parameters.
-
-        Args:
-            name (str): ID of the sink modelification.
-            upstream_name (list[str]): ID of the sink modelification.
-            method (LoadMethod): Type of sink load mode.
-            location (str): URI that identifies where to load data in the modelified format.
-            schema_location (str): URI that identifies where to load schema.
-            options (dict[str, Any]): Options for the sink input.
-        """
-        self.name = name
-        self.upstream_name = upstream_name
-        self.method = method
-        self.location = location
-        self.schema_location = schema_location
-        self.options = options
-
-    @property
-    def name(self) -> str:
-        """Get the name of the loading operation."""
-        return self._name
-
-    @name.setter
-    def name(self, value: str) -> None:
-        """Set the name of the loading operation."""
-        self._name = value
-
-    @property
-    def upstream_name(self) -> str:
-        """Get the name of the upstream data source."""
-        return self._upstream_name
-
-    @upstream_name.setter
-    def upstream_name(self, value: str) -> None:
-        """Set the name of the upstream data source."""
-        self._upstream_name = value
-
-    @property
-    def method(self) -> LoadMethod:
-        """Get the loading method (batch or streaming)."""
-        return self._method
-
-    @method.setter
-    def method(self, value: LoadMethod) -> None:
-        """Set the loading method (batch or streaming)."""
-        self._method = value
-
-    @property
-    def location(self) -> str:
-        """Get the destination URI for the loaded data."""
-        return self._location
-
-    @location.setter
-    def location(self, value: str) -> None:
-        """Set the destination URI for the loaded data."""
-        self._location = value
-
-    @property
-    def schema_location(self) -> str | None:
-        return self._schema_location
-
-    @schema_location.setter
-    def schema_location(self, value: str | None) -> None:
-        self._schema_location = value
-
-    @property
-    def options(self) -> dict[str, str]:
-        return self._options
-
-    @options.setter
-    def options(self, value: dict[str, str]) -> None:
-        self._options = value
-
-    @classmethod
-    @abstractmethod
-    def from_dict(cls, dict_: dict[str, Any]) -> Self:
-        """
-        Create a loading model from a configuration dictionary.
-
-        Args:
-            dict_: Configuration dictionary containing loading parameters
-
-        Returns:
-            An initialized loading model
-
-        Raises:
-            DictKeyError: If required keys are missing from the configuration
-        """
 
 
 class Load(Generic[LoadModelT], ABC):
@@ -195,26 +55,6 @@ class Load(Generic[LoadModelT], ABC):
         self.model = model
         self.data_registry = DataFrameRegistry()
         self.streaming_query_registry = StreamingQueryRegistry()
-
-    @property
-    def model(self) -> LoadModelT:
-        """Get the loading model configuration."""
-        return self._model
-
-    @model.setter
-    def model(self, value: LoadModelT) -> None:
-        """Set the loading model configuration."""
-        self._model = value
-
-    @property
-    def data_registry(self) -> DataFrameRegistry:
-        """Get the data registry containing data to be loaded."""
-        return self._data_registry
-
-    @data_registry.setter
-    def data_registry(self, value: DataFrameRegistry) -> None:
-        """Set the data registry containing data to be loaded."""
-        self._data_registry = value
 
     @classmethod
     def from_dict(cls, dict_: dict[str, Any]) -> Self:
@@ -280,88 +120,12 @@ class Load(Generic[LoadModelT], ABC):
         self._load_schema()
 
 
-class LoadModelFile(LoadModel):
-    """Abstract base class for file-based load models."""
-
-    def __init__(
-        self,
-        name: str,
-        upstream_name: str,
-        method: LoadMethod,
-        mode: LoadMode,
-        data_format: LoadFormat,
-        location: str,
-        schema_location: str | None,
-        options: dict[str, str],
-    ) -> None:
-        super().__init__(
-            name=name,
-            upstream_name=upstream_name,
-            method=method,
-            location=location,
-            schema_location=schema_location,
-            options=options,
-        )
-        self.mode = mode
-        self.data_format = data_format
-
-    @property
-    def mode(self) -> LoadMode:
-        return self._mode
-
-    @mode.setter
-    def mode(self, value: LoadMode) -> None:
-        self._mode = value
-
-    @property
-    def data_format(self) -> LoadFormat:
-        return self._data_format
-
-    @data_format.setter
-    def data_format(self, value: LoadFormat) -> None:
-        self._data_format = value
-
-    @classmethod
-    def from_dict(cls, dict_: dict[str, Any]) -> Self:
-        """
-        Create a LoadModelFilePyspark object from a dict_ dictionary.
-
-        Args:
-            dict_ (dict[str, Any]): The dict_ dictionary.
-
-        Returns:
-            LoadModelFilePyspark: LoadModelFilePyspark object.
-        """
-        try:
-            name = dict_[NAME]
-            upstream_name = dict_[UPSTREAM_NAME]
-            method = LoadMethod(dict_[METHOD])
-            mode = LoadMode(dict_[MODE])
-            data_format = LoadFormat(dict_[DATA_FORMAT])
-            location = dict_[LOCATION]
-            schema_location = dict_.get(SCHEMA_LOCATION, None)
-            options = dict_.get(OPTIONS, {})
-        except KeyError as e:
-            raise DictKeyError(key=e.args[0], dict_=dict_) from e
-
-        return cls(
-            name=name,
-            upstream_name=upstream_name,
-            method=method,
-            mode=mode,
-            data_format=data_format,
-            location=location,
-            schema_location=schema_location,
-            options=options,
-        )
-
-
 @LoadRegistry.register(LoadFormat.PARQUET)
 @LoadRegistry.register(LoadFormat.JSON)
 @LoadRegistry.register(LoadFormat.CSV)
 class LoadFile(Load[LoadModelFile]):
     """
-    Concrete class for file loadion using PySpark DataFrame.
+    Concrete class for file loading using PySpark DataFrame.
     """
 
     load_model_concrete = LoadModelFile
